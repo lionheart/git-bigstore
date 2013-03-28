00d957419f597e17a39f03834a11f3b4b556acd6
Dan Loewenherz <dloewenherz@gmail.com>
2013-03-27 20:26:47 -0700

    add bigstore metadata files

---
+++ bigstore/metadata.py
@@ -0,0 +1,7 @@
+__version__ = "0.1"
+__author__ = "Dan Loewenherz"
+__copyright__ = "Copyright 2013, Aurora Software"
+__maintainer__ = "Dan Loewenherz"
+__email__ = "dan@aurora.io"
+__license__ = "Apache 2.0"
+
---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env/python
+
+import os
+from bigstore import metadata
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name = 'git-bigstore',
+    version = metadata.__version__,
+    license = metadata.__license__,
+    description = "Track big files with Git.",
+    author = metadata.__author__,
+    author_email = metadata.__email__,
+    packages=[
+        'bigstore',
+    ],
+    install_requires=['gitpython==0.3.2.RC1'],
+)
+
