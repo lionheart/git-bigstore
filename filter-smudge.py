f2d9f5537ea6703dcd88c8bf7d6fa76d9e072639
Dan Loewenherz <dloewenherz@gmail.com>
2013-03-27 20:17:19 -0700

    add sync script

--- filter-smudge.py
+++ filter-smudge.py
@@ -6,22 +6,23 @@ from git import Git
 import os
 import re
 
-hexdigest = sys.stdin.read()
+contents = sys.stdin.read()
 thirty_two_hex = re.compile(r'^bigfile\$[a-f0-9]{32}')
 
 g = Git('.')
 git_directory = g.rev_parse(git_dir=True)
-source_filename = os.path.join(git_directory, "storage/objects", hexdigest)
 
-if thirty_two_hex.match(hexdigest):
+if thirty_two_hex.match(contents):
+    _, hexdigest = contents.split('$')
+    source_filename = os.path.join(git_directory, "storage/objects", hexdigest)
     try:
         file = open(source_filename, 'rb')
     except IOError:
         sys.stderr.write("couldn't find file, saving placeholder \n")
-        sys.stdout.write(hexdigest)
+        sys.stdout.write(contents)
     else:
         for line in file:
             sys.stdout.write(line)
 else:
-    sys.stdout.write(hexdigest)
+    sys.stdout.write(contents)
 
