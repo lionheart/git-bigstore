6e80ab3e564de3b02213341109f91039297a6461
Dan Loewenherz <dloewenherz@gmail.com>
2013-04-06 21:33:55 -0500

    change file

--- bigstore/backends/s3.py
+++ bigstore/backends/s3.py
@@ -9,11 +9,11 @@ class S3Backend(object):
         self.bucket = boto.s3.bucket.Bucket(self.conn, bucket_name)
 
     def push(self, file, hash):
-        key = boto.s3.key.Key(self.bucket, hash)
         # key.set_contents_from_file(file, cb=transfer_callback(filename))
+        key = boto.s3.key.Key(self.bucket, hash)
         key.set_contents_from_file(file)
 
-    def pull(self, destination, hash, cb=None):
+    def pull(self, file, hash, cb=None):
         # key.get_contents_to_file(file, cb=transfer_callback(filename))
         key = boto.s3.key.Key(self.bucket, hash)
         key.get_contents_to_file(file)
