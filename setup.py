b2c958ef011a56cb0ab4229ff7f59f6201b0ba8d
Dan Loewenherz <dloewenherz@gmail.com>
2015-08-17 20:00:18 -0500

    prevent ImportErrors on installation

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
 
 import os
-from bigstore import metadata
 
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
 
+metadata = {}
+execfile("bigstore/metadata.py", metadata)
+
 setup(
     name='git-bigstore',
-    version=metadata.__version__,
-    license=metadata.__license__,
+    version=metadata['__version__'],
+    license=metadata['__license__'],
     description="Track big files with Git.",
-    author=metadata.__author__,
-    author_email=metadata.__email__,
+    author=metadata['__author__'],
+    author_email=metadata['__email__'],
     url="http://aurora.io/git-bigstore",
     packages=[
         'bigstore.backends',
